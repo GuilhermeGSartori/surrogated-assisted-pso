3fdd215c69c1e24b00270277c2dcf48777d6fa55
Guilherme <guigsartori@hotmail.com>
2026-09-12 18:41:03 -0300

    Minor function header fix

--- source/aux/include/trainer.h
+++ source/aux/include/trainer.h
@@ -8,4 +8,4 @@ void writeDatasetHeader(std::ofstream& file, std::size_t n_relays);
 void writeNodesHeader(std::ofstream& file);
 void writeNodesScenario(std::ofstream& file, std::size_t scenario_id, const Scenario& scenario);
 void writeDatasetRow(std::ofstream& file, std::size_t id, std::size_t sample_id, const Scenario& scenario, const FixedSizeVector<Coordinates>& relays, double fitness);
-void generateDataset(Scenario& scenario);
+void generateDataset(TrainingScenario& scenario);
